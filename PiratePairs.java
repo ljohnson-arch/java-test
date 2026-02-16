69edc095d877c9299e197132a0c7278bb1d6ff68
Lorenzo Johnson <ljohnson@brooksschool.org>
2026-02-16 18:21:16 -0500

    made an array of players in the pirate pares file

--- PiratePairs.java
+++ PiratePairs.java
@@ -1,11 +1,13 @@
 public class PiratePairs{
     public static void main(String[] args){
-         GameDeck deck = new GameDeck(55);
-         int numOfPlayers = 5;
-
-        for (int i = 1; i <= numOfPlayers; i++){
-             Player player = new Player(i);
+        GameDeck deck = new GameDeck(55);
+        int numOfPlayers = 5; // chsnge to scanner 
+        Player[] player = new Player[numOfPlayers];
+        
+        for (int i = 0; i < numOfPlayers; i++){
+            player[i] = new Player(i + 1);
         }
+        
          
         
          deck.printDeck();
