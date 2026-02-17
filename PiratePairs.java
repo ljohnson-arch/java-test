f986510e50a32c2a352e406965816365ab4d82a8
Lorenzo Johnson <ljohnson@brooksschool.org>
2026-02-17 08:51:30 -0500

    game is complete

--- PiratePairs.java
+++ PiratePairs.java
@@ -1,18 +1,20 @@
-public class PiratePairs{
-    public static void main(String[] args){
-
-        int numOfPlayers = 5; // chsnge to scanner 
+import java.util.Scanner;
+public class PiratePairs {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter Number of PLayers: ");
+        int numOfPlayers = input.nextInt();
         int losingScore = (60 / numOfPlayers) + 1;
 
         GameDeck deck = new GameDeck(55);
         Player[] player = new Player[numOfPlayers];
-        
-        for (int i = 0; i < numOfPlayers; i++){
+
+        for (int i = 0; i < numOfPlayers; i++) {
             player[i] = new Player(i + 1);
         }
-        
-        //first round everyone player gets a card.
-        for (int i = 0; i < numOfPlayers; i++){
+
+        // first round everyone player gets a card.
+        for (int i = 0; i < numOfPlayers; i++) {
             int card = deck.drawCard();
             player[i].addCard(card);
 
@@ -22,96 +24,94 @@ public class PiratePairs{
         int playersIn = numOfPlayers;
         int turn = 0;
 
-        //while loop here
-        while(playersIn > 1){
-             Player current = player[turn % numOfPlayers];
+        // while loop here
+        while (playersIn > 1) {
+            Player current = player[turn % numOfPlayers];
 
-            if(!current.isOut()){
+            if (!current.isOut()) {
 
                 System.out.println("Player " + current.getId() + "'s turn");
 
-                if(current.handSize() == 0){
-                     int card = deck.drawCard();
-                     System.out.println("Draws " + card);
-                     current.addCard(card);
+                if (current.handSize() == 0) {
+                    int card = deck.drawCard();
+                    System.out.println("Draws " + card);
+                    current.addCard(card);
 
                 } else {
-                    //strat if more than 3 cards takes lowest
+                    // strat if more than 3 cards takes lowest
 
-                    if (current.handSize() > 3){
+                    if (current.handSize() > 3) {
 
                         int lowest = findLowest(player);
                         System.out.println("Folds and takes " + lowest + " points");
 
                         current.addScore(lowest);
 
-                        //discard hand
+                        // discard hand
                         discardHand(current, deck);
                         current.clearHand();
-                        
-                    } else{
+
+                    } else {
 
                         int card = deck.drawCard();
                         System.out.println("Draws " + card);
 
-                        if (current.hasCard(card)){
+                        if (current.hasCard(card)) {
                             System.out.println("Pair! add " + card + " to score");
                             current.addScore(card);
 
-                            //discard hand and pair card
+                            // discard hand and pair card
                             deck.addToDiscard(card);
                             discardHand(current, deck);
 
                             current.clearHand();
-                        } else{
+                        } else {
                             current.addCard(card);
                         }
                     }
                 }
 
-                //check if player is out
-                if (current.getScore() > losingScore){
+                // check if player is out
+                if (current.getScore() > losingScore) {
                     System.out.println("Player " + current.getId() + " is OUT!");
                     current.setOut(true);
                     playersIn--;
                 }
             }
-            turn++; 
-    }
-
-        
-           
-    
-    //declare winner
-    for(Player p : player){
-        if(!p.isOut()){
-            System.out.println("Playe " + p.getId() + " Wins!");
+            turn++;
         }
+
+        // declare winner
+        for (Player p : player) {
+            if (!p.isOut()) {
+                System.out.println("Playe " + p.getId() + " Wins!");
+            }
+        }
+        input.close();
     }
-}
 
+    public static int findLowest(Player[] player) {
 
-     public static int findLowest(Player[] player){
+        int lowest = 11;
 
-            int lowest = 11;
-
-            for (Player p: player){
-                if(!p.isOut() && p.handSize() > 0){
-                    int min = p.lowestCard();
-                    if (min < lowest){
-                        lowest = min;
-                    }
+        for (Player p : player) {
+            if (!p.isOut() && p.handSize() > 0) {
+                int min = p.lowestCard();
+                if (min < lowest) {
+                    lowest = min;
                 }
             }
-            return lowest;
-         }
+        }
+        return lowest;
+    }
 
-    public static void discardHand(Player player, GameDeck deck){
+    public static void discardHand(Player player, GameDeck deck) {
 
         int[] hand = player.getHand();
 
-        for (int i = 0; i < player.handSize(); i++){
+        for (int i = 0; i < player.handSize(); i++) {
             deck.addToDiscard(hand[i]);
         }
+        
     }
- }
+}
